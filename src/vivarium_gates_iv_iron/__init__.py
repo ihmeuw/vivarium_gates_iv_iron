0680fdf978cc26fa040dac8e3722ee992ac90186
Matthew Kappel <mkappel@uw.edu>
2021-12-22 16:59:50 -0800

    "Template commit"

---
+++ src/vivarium_gates_iv_iron/__init__.py
@@ -0,0 +1,5 @@
+"""vivarium_gates_iv_iron
+
+Research repository for the vivarium_gates_iv_iron project.
+
+"""
---
+++ src/vivarium_gates_iv_iron/paths.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import vivarium_gates_iv_iron
+from vivarium_gates_iv_iron.constants import metadata
+
+BASE_DIR = Path(vivarium_gates_iv_iron.__file__).resolve().parent
+
+ARTIFACT_ROOT = Path(f"/share/costeffectiveness/artifacts/{metadata.PROJECT_NAME}/")
+MODEL_SPEC_DIR = BASE_DIR / 'model_specifications'
+RESULTS_ROOT = Path(f'/share/costeffectiveness/results/{metadata.PROJECT_NAME}/')
