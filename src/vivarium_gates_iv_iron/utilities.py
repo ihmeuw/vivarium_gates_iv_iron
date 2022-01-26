7e578d9446cd06dbb027faae1cb0ab92b61f9268
Matthew Kappel <mkappel@uw.edu>
2022-01-26 15:08:40 -0800

    Add random variable getter fn

--- src/vivarium_gates_iv_iron/utilities.py
+++ src/vivarium_gates_iv_iron/utilities.py
@@ -1,10 +1,12 @@
 import click
+import numpy as np
 import pandas as pd
 
 from typing import NamedTuple, Union, List
 from pathlib import Path
 from loguru import logger
 
+from vivarium.framework.randomness import get_hash
 from vivarium_public_health.risks.data_transformations import pivot_categorical
 
 from vivarium_gates_iv_iron.constants import metadata
@@ -55,29 +57,38 @@ def delete_if_exists(*paths: Union[Path, List[Path]], confirm=False):
             p.unlink()
 
 
-def read_data_by_draw(artifact_path: str, key: str, draw: int) -> pd.DataFrame:
-    """Reads data from the artifact on a per-draw basis. This
-    is necessary for Low Birthweight Short Gestation (LBWSG) data.
+# def read_data_by_draw(artifact_path: str, key: str, draw: int) -> pd.DataFrame:
+#     """Reads data from the artifact on a per-draw basis. This
+#     is necessary for Low Birthweight Short Gestation (LBWSG) data.
+#
+#     Parameters
+#     ----------
+#     artifact_path
+#         The artifact to read from.
+#     key
+#         The entity key associated with the data to read.
+#     draw
+#         The data to retrieve.
+#
+#     """
+#     key = key.replace(".", "/")
+#     with pd.HDFStore(artifact_path, mode="r") as store:
+#         index = store.get(f"{key}/index")
+#         draw = store.get(f"{key}/draw_{draw}")
+#     draw = draw.rename("value")
+#     data = pd.concat([index, draw], axis=1)
+#     data = data.drop(columns="location")
+#     data = pivot_categorical(data)
+#     data[
+#         project_globals.LBWSG_MISSING_CATEGORY.CAT
+#     ] = project_globals.LBWSG_MISSING_CATEGORY.EXPOSURE
+#     return data
 
-    Parameters
-    ----------
-    artifact_path
-        The artifact to read from.
-    key
-        The entity key associated with the data to read.
-    draw
-        The data to retrieve.
 
-    """
-    key = key.replace(".", "/")
-    with pd.HDFStore(artifact_path, mode="r") as store:
-        index = store.get(f"{key}/index")
-        draw = store.get(f"{key}/draw_{draw}")
-    draw = draw.rename("value")
-    data = pd.concat([index, draw], axis=1)
-    data = data.drop(columns="location")
-    data = pivot_categorical(data)
-    data[
-        project_globals.LBWSG_MISSING_CATEGORY.CAT
-    ] = project_globals.LBWSG_MISSING_CATEGORY.EXPOSURE
-    return data
+def get_random_variable_draws(columns: pd.Index, seed: str, distribution) -> pd.Series:
+    return pd.Series([get_random_variable(x, seed, distribution) for x in range(0, columns.size)], index=columns)
+
+
+def get_random_variable(draw: int, seed: str, distribution) -> pd.Series:
+    np.random.seed(get_hash(f'{seed}_draw_{draw}'))
+    return distribution.rvs()
