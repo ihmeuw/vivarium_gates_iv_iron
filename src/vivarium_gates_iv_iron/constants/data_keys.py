f19ff073b8b974c4b33ce67f477fd40aeedbdd15
albrja <albrja@uw.edu>
2022-01-28 13:23:23 -0800

    Commenting out more stuff

--- src/vivarium_gates_iv_iron/constants/data_keys.py
+++ src/vivarium_gates_iv_iron/constants/data_keys.py
@@ -33,18 +33,18 @@ POPULATION = __Population()
 class __Pregnancy(NamedTuple):
 
     # Keys that will be loaded into the artifact. must have a colon type declaration
-    PREGNANCY_PREVALENCE: TargetString = TargetString(
-        "cause.pregnancy.prevalence"
-    )
-    PREGNANCY_INCIDENCE_RATE: TargetString = TargetString(
-        "cause.pregnancy.incidence_rate"
-    )
-    PREGNANCY_REMISSION_RATE: TargetString = TargetString(
-        "cause.pregnancy.remission_rate"
-    )
-    DISABILITY_WEIGHT: TargetString = TargetString(
-        "cause.pregnancy.disability_weight"
-    )
+    # PREGNANCY_PREVALENCE: TargetString = TargetString(
+    #     "cause.pregnancy.prevalence"
+    # )
+    # PREGNANCY_INCIDENCE_RATE: TargetString = TargetString(
+    #     "cause.pregnancy.incidence_rate"
+    # )
+    # PREGNANCY_REMISSION_RATE: TargetString = TargetString(
+    #     "cause.pregnancy.remission_rate"
+    # )
+    # DISABILITY_WEIGHT: TargetString = TargetString(
+    #     "cause.pregnancy.disability_weight"
+    # )
     ASFR: TargetString = TargetString('covariate.age_specific_fertility_rate.estimate'
     )
     SBR: TargetString = TargetString("cause.pregnancy.still_birth_to_live_birth_ratio"
@@ -53,15 +53,15 @@ class __Pregnancy(NamedTuple):
     )
     INCIDENCE_C995: TargetString = TargetString("cause.maternal_abortion_and_miscarriage.incidence_rate"
     )
-    EMR: TargetString = TargetString("cause.pregnancy.excess_mortality_rate")
-    CSMR: TargetString = TargetString(
-        "cause.pregnancy.cause_specific_mortality_rate"
-    )
-    RESTRICTIONS: TargetString = TargetString("cause.pregnancy.restrictions")
-
-    # Useful keys not for the artifact - distinguished by not using the colon type declaration
-    RAW_DISEASE_PREVALENCE = TargetString("sequela.raw_disease.prevalence")
-    RAW_DISEASE_INCIDENCE_RATE = TargetString("sequela.raw_disease.incidence_rate")
+    # EMR: TargetString = TargetString("cause.pregnancy.excess_mortality_rate")
+    # CSMR: TargetString = TargetString(
+    #     "cause.pregnancy.cause_specific_mortality_rate"
+    # )
+    # RESTRICTIONS: TargetString = TargetString("cause.pregnancy.restrictions")
+    #
+    # # Useful keys not for the artifact - distinguished by not using the colon type declaration
+    # RAW_DISEASE_PREVALENCE = TargetString("sequela.raw_disease.prevalence")
+    # RAW_DISEASE_INCIDENCE_RATE = TargetString("sequela.raw_disease.incidence_rate")
 
     @property
     def name(self):
