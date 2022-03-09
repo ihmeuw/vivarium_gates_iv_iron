38834892541822a7ff3d938b4dc3805f62d5fe6f
albrja <albrja@uw.edu>
2022-03-09 09:38:29 -0800

    Changing keys per feedback for hemoglobin to risk factor

--- src/vivarium_gates_iv_iron/constants/data_keys.py
+++ src/vivarium_gates_iv_iron/constants/data_keys.py
@@ -130,8 +130,8 @@ MATERNAL_HEMORRHAGE = __MaternalHemorrhage()
 
 
 class _Hemoglobin(NamedTuple):
-    MEAN: TargetString = TargetString("cause.hemoglobin.mean")
-    STANDARD_DEVIATION: TargetString = TargetString("cause.hemoglobin.standard_deviation")
+    MEAN: TargetString = TargetString("risk_factor.hemoglobin.mean")
+    STANDARD_DEVIATION: TargetString = TargetString("risk_factor.hemoglobin.standard_deviation")
 
     @property
     def name(self):
