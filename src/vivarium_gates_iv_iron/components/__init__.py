37ef60b04ce99773e5fbaac6c7859a655bbcea20
albrja <albrja@uw.edu>
2022-03-16 15:43:07 -0700

    Format change for obs init.py

--- src/vivarium_gates_iv_iron/components/__init__.py
+++ src/vivarium_gates_iv_iron/components/__init__.py
@@ -1,4 +1,5 @@
-
-from vivarium_gates_iv_iron.components.observers import MortalityObserver, DisabilityObserver, PregnancyObserver, \
-    MaternalDisordersObserver, MaternalHemorrhageObserver
+from vivarium_gates_iv_iron.components.observers import (
+    MortalityObserver, DisabilityObserver, PregnancyObserver,
+    MaternalDisordersObserver, MaternalHemorrhageObserver,
+)
 from vivarium_gates_iv_iron.components.pregnancy import Pregnancy
