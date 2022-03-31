1ed99fe6aa772191bcd5f98fdc0870f85e975f18
albrja <37345113+albrja@users.noreply.github.com>
2022-03-31 15:28:39 -0700

    Model 3: Maternal hemoglobin and anemia risk exposure implementation (#34)
    Model 3: Maternal hemoglobin and anemia risk exposure implementation
    
    Description
    Implementation of hemoglobin sampling and distribution weighting by location along with pregnancy adjustment factors.  Also implements anemia level risk exposure.
    - *Category*: Implementation
    - *JIRA issue*: [MIC-2849](https://jira.ihme.washington.edu/browse/MIC-2849)
    - *Research reference*: https://vivarium-research.readthedocs.io/en/latest/gbd2019_models/hemoglobin_anemia_and_iron_deficiency/hemoglobin/index.html#hemoglobin-model
    
    Verification and Testing
    Ran interactive simulation and verified hemoglobin shift between pregnant and not pregnant simulants.  Also verified anemia levels were assigned to simulants based on hemoglobin exposure.

--- src/vivarium_gates_iv_iron/components/hemoglobin.py
+++ src/vivarium_gates_iv_iron/components/hemoglobin.py
@@ -6,7 +6,8 @@ from vivarium.framework.engine import Builder
 from vivarium.framework.population import SimulantData
 
 
-from vivarium_gates_iv_iron.constants.data_values import HEMOGLOBIN_DISTRIBUTION_PARAMETERS
+from vivarium_gates_iv_iron.constants.data_values import HEMOGLOBIN_DISTRIBUTION_PARAMETERS, HEMOGLOBIN_THRESHOLD_DATA, ANEMIA_DISABILITY_WEIGHTS
+
 from vivarium_gates_iv_iron.constants import data_keys
 
 
@@ -25,34 +26,29 @@ class Hemoglobin:
         self.randomness = builder.randomness.get_stream(self.name)
         self.columns_created = ["country", "hemoglobin_distribution_propensity", "hemoglobin_percentile"]
         # load data
-        mean = builder.data.load(data_keys.HEMOGLOBIN.MEAN)
-        stddev = builder.data.load(data_keys.HEMOGLOBIN.STANDARD_DEVIATION)
-        # TODO replace --->>>
-        location_weights = mean.copy().drop(columns=["value"])
-        # lookup data, categorical column "country"
-        mean_dfs, stddev_dfs = [], []
-        # TODO have this in artifact and correct the location weights
-        for country in ["Bangladesh", "Pakistan", "India"]:
-            loc_mean = mean.copy()
-            loc_stddev = stddev.copy()
-            loc_mean["country"] = country
-            loc_stddev["country"] = country
-            mean_dfs.append(loc_mean)
-            stddev_dfs.append(loc_stddev)
-            location_weights[country] = 1/3  # will be pop of country over pop of region
+        # TODO: do this s/location/country in the loader?
+        mean = builder.data.load(data_keys.HEMOGLOBIN.MEAN).rename(columns={"location": "country"})
+        stddev = builder.data.load(data_keys.HEMOGLOBIN.STANDARD_DEVIATION).rename(columns={"location": "country"})
+        self.location_weights = builder.data.load(data_keys.POPULATION.PLW_LOCATION_WEIGHTS).rename(columns={"location": "country"})
         index_columns = ["country", "sex", "age_start", 'age_end', 'year_start', 'year_end']
-        mean = pd.concat(mean_dfs).set_index(index_columns)["value"].rename("mean")
-        stddev = pd.concat(stddev_dfs).set_index(index_columns)["value"].rename("stddev")
-        # <<<--- TODO replace
+        mean = mean.set_index(index_columns)["value"].rename("mean")
+        stddev = stddev.set_index(index_columns)["value"].rename("stddev")
         distribution_parameters = pd.concat([mean, stddev], axis=1).reset_index()
         self.distribution_parameters = builder.value.register_value_producer("hemoglobin.exposure_parameters",
             source=builder.lookup.build_table(distribution_parameters, key_columns=["sex", "country"], parameter_columns=["age", "year"]),
                                                                              requires_columns=["age", "sex", "country"])
-        self.location_weights = builder.lookup.build_table(location_weights, key_columns=["sex"],
-                                                           parameter_columns=["age", "year"])
+
         self.hemoglobin = builder.value.register_value_producer("hemoglobin.exposure", source=self.hemoglobin_source,
                                                                 requires_values=["hemoglobin.exposure_parameters"],
                                                                 requires_streams=[self.name])
+
+        self.thresholds = builder.lookup.build_table(HEMOGLOBIN_THRESHOLD_DATA,
+                                                     key_columns=["sex", "pregnancy_status"],
+                                                     parameter_columns=["age"])
+
+        self.anemia_levels = builder.value.register_value_producer("anemia_levels", source=self.anemia_source,
+                                                                   requires_values=["hemoglobin.exposure"])
+
 
         builder.population.initializes_simulants(self.on_initialize_simulants,
                                                  creates_columns=self.columns_created,
@@ -62,11 +58,13 @@ class Hemoglobin:
         builder.event.register_listener("time_step", self.on_time_step)
 
     def on_initialize_simulants(self, pop_data: SimulantData) -> None:
-        location_weights = self.location_weights(pop_data.index)
-        pop_update = pd.DataFrame({"country": self.randomness.choice(pop_data.index, choices=location_weights.columns.tolist(),
-                                                                     p=location_weights, additional_key="country"),
-                                   "hemoglobin_distribution_propensity": self.randomness.get_draw(pop_data.index,
-                                                                                     additional_key="hemoglobin_distribution_propensity"),
+        pop_update = pd.DataFrame({"country": self.randomness.choice(pop_data.index,
+                                                                     choices=self.location_weights.country.to_list(),
+                                                                     p=self.location_weights.value.to_list(),
+                                                                     additional_key="country"),
+                                   "hemoglobin_distribution_propensity": self.randomness.get_draw(
+                                       pop_data.index,
+                                       additional_key="hemoglobin_distribution_propensity"),
                                    "hemoglobin_percentile": self.randomness.get_draw(pop_data.index, additional_key="hemoglobin_percentile")},
                                   index=pop_data.index)
         self.population_view.update(pop_update)
@@ -74,7 +72,7 @@ class Hemoglobin:
     def on_time_step(self, event):
         self.distribution_parameters(event.index)
         self.hemoglobin(event.index)
-        breakpoint()
+        self.anemia_levels(event.index)
         return
 
     def hemoglobin_source(self, idx: pd.Index) -> pd.Series:
@@ -82,6 +80,12 @@ class Hemoglobin:
         pop = self.population_view.get(idx)
         return self.sample_from_hemoglobin_distribution(pop["hemoglobin_distribution_propensity"], pop["hemoglobin_percentile"], distribution_parameters)
 
+    def anemia_source(self, index: pd.Index) -> pd.Series:
+        hemoglobin_level = self.hemoglobin(index)
+        thresholds = self.thresholds(index)
+        choice_index = (hemoglobin_level.values[np.newaxis].T < thresholds).sum(axis=1)
+
+        return pd.Series(np.array(["none", "mild", "moderate", "severe"])[choice_index], index=index, name="anemia_levels")
 
     @staticmethod
     def _gamma_ppf(propensity, mean, sd):
@@ -121,6 +125,16 @@ class Hemoglobin:
         gamma = propensity_distribution < 0.4
         gumbel = ~gamma
         ret_val = pd.Series(index=propensity_distribution.index, name="value")
-        ret_val.loc[gamma] = self._gamma_ppf(propensity.loc[gamma], mean, sd)
-        ret_val.loc[gumbel] = self._mirrored_gumbel_ppf(propensity.loc[gumbel], mean, sd)
+        ret_val.loc[gamma] = self._gamma_ppf(propensity, mean, sd)[gamma]
+        ret_val.loc[gumbel] = self._mirrored_gumbel_ppf(propensity, mean, sd)[gumbel]
         return ret_val
+
+    def disability_weight(self, index: pd.Index) -> pd.Series:
+        hemoglobin_level = self.hemoglobin(index)
+        thresholds = self.thresholds(index)
+        choice_index = (hemoglobin_level.values[np.newaxis].T < thresholds).sum(axis=1)
+        anemia_levels = pd.Series(np.array(["none", "mild", "moderate", "severe"])[choice_index], index=index,
+                                 name="anemia_levels")
+        return anemia_levels.map(ANEMIA_DISABILITY_WEIGHTS)
+
+
