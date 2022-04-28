f2b21835cce3d561715c810ecce502c51976ee03
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-04-28 13:51:21 -0700

    create lbwsg exposure component

--- src/vivarium_gates_iv_iron/components/__init__.py
+++ src/vivarium_gates_iv_iron/components/__init__.py
@@ -1,10 +1,11 @@
-from vivarium_gates_iv_iron.components.observers import (
-    MortalityObserver,
-    DisabilityObserver,
-    PregnancyObserver,
-    MaternalDisordersObserver,
-    MaternalHemorrhageObserver,
-    HemoglobinObserver,
-    AnemiaObserver,
-)
+from vivarium_gates_iv_iron.components.child import LBWSGExposure
+# from vivarium_gates_iv_iron.components.observers import (
+#     MortalityObserver,
+#     DisabilityObserver,
+#     PregnancyObserver,
+#     MaternalDisordersObserver,
+#     MaternalHemorrhageObserver,
+#     HemoglobinObserver,
+#     AnemiaObserver,
+# )
 from vivarium_gates_iv_iron.components.pregnancy import Pregnancy
