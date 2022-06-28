6977eea320b1ef655aa82ec157a2953956937bbe
Matthew Kappel <mkappel@uw.edu>
2022-06-27 18:54:26 -0700

    Do black on constants, loader.py too fraught for black atm

--- src/vivarium_gates_iv_iron/constants/data_keys.py
+++ src/vivarium_gates_iv_iron/constants/data_keys.py
@@ -31,21 +31,21 @@ POPULATION = __Population()
 
 
 class __Pregnancy(NamedTuple):
-    ASFR: str = 'covariate.age_specific_fertility_rate.estimate'
-    SBR: str = 'covariate.stillbirth_to_live_birth_ratio.estimate'
+    ASFR: str = "covariate.age_specific_fertility_rate.estimate"
+    SBR: str = "covariate.stillbirth_to_live_birth_ratio.estimate"
     LIVE_BIRTHS_BY_SEX: str = "covariate.live_births_by_sex.estimate"
     INCIDENCE_RATE_MISCARRIAGE: str = "cause.maternal_abortion_and_miscarriage.incidence_rate"
     INCIDENCE_RATE_ECTOPIC: str = "cause.ectopic_pregnancy.incidence_rate"
     PREVALENCE: str = "covariate.pregnancy_prevalence.estimate"
     CONCEPTION_RATE: str = "covariate.conception_rate.estimate"
-    CHILD_OUTCOME_PROBABILITIES: str = 'covariate.child_outcome_probability.estimate'
+    CHILD_OUTCOME_PROBABILITIES: str = "covariate.child_outcome_probability.estimate"
     PREGNANT_LACTATING_WOMEN_LOCATION_WEIGHTS: str = (
         "covariate.pregnant_and_lactating_women_location_weights.estimate"
     )
     WOMEN_REPRODUCTIVE_AGE_LOCATION_WEIGHTS: str = (
         "covariate.women_of_reproductive_age_location_weights.estimate"
     )
-    HEMOGLOBIN_CORRECTION_FACTORS: str = 'covariate.hemoglobin_correction_factors.estimate'
+    HEMOGLOBIN_CORRECTION_FACTORS: str = "covariate.hemoglobin_correction_factors.estimate"
     PAF_STILLBIRTH_PROBABILITY_ATTRIBUTABLE_TO_HEMOGLOBIN: str = "risk_factor.hemoglobin_on_stillbirth.paf"
     RR_STILLBIRTH_PROBABILITY_ATTRIBUTABLE_TO_HEMOGLOBIN: str = "risk_factor.hemoglobin_on_stillbirth.relative_risk"
 
@@ -68,12 +68,12 @@ class __MaternalDisorders(NamedTuple):
     HEMORRHAGE_INCIDENCE_RATE: str = "cause.maternal_hemorrhage.incidence_rate"
     YLDS: str = "cause.maternal_disorders.ylds"
 
-    PROBABILITY_FATAL: str = 'covariate.probability_fatal_maternal_disorder.estimate'
-    PROBABILITY_NONFATAL: str = 'covariate.probability_nonfatal_maternal_disorder.estimate'
+    PROBABILITY_FATAL: str = "covariate.probability_fatal_maternal_disorder.estimate"
+    PROBABILITY_NONFATAL: str = "covariate.probability_nonfatal_maternal_disorder.estimate"
     PROBABILITY_HEMORRHAGE: str = "covariate.probability_maternal_hemorrhage.estimate"
 
-    RR_MATERNAL_HEMORRHAGE_ATTRIBUTABLE_TO_HEMOGLOBIN: str = 'risk_factor.hemoglobin_on_maternal_hemorrhage.relative_risk'
-    PAF_MATERNAL_HEMORRHAGE_ATTRIBUTABLE_TO_HEMOGLOBIN: str = 'risk_factor.hemoglobin_on_maternal_hemorrhage.paf'
+    RR_MATERNAL_HEMORRHAGE_ATTRIBUTABLE_TO_HEMOGLOBIN: str = "risk_factor.hemoglobin_on_maternal_hemorrhage.relative_risk"
+    PAF_MATERNAL_HEMORRHAGE_ATTRIBUTABLE_TO_HEMOGLOBIN: str = "risk_factor.hemoglobin_on_maternal_hemorrhage.paf"
 
     RR_MATERNAL_DISORDER_ATTRIBUTABLE_TO_HEMOGLOBIN: str = "risk_factor.hemoglobin_on_maternal_disorder.relative_risk"
     PAF_MATERNAL_DISORDER_ATTRIBUTABLE_TO_HEMOGLOBIN: str = "risk_factor.hemoglobin_on_maternal_disorder.paf"
@@ -92,17 +92,20 @@ MATERNAL_DISORDERS = __MaternalDisorders()
 
 class _Hemoglobin(NamedTuple):
     MEAN: TargetString = TargetString("risk_factor.hemoglobin.mean")
-    STANDARD_DEVIATION: TargetString = TargetString("risk_factor.hemoglobin.standard_deviation")
+    STANDARD_DEVIATION: TargetString = TargetString(
+        "risk_factor.hemoglobin.standard_deviation"
+    )
     PREGNANT_PROPORTION_WITH_HEMOGLOBIN_BELOW_70: TargetString = TargetString(
-        "risk_factor.hemoglobin.pregnant_proportion_below_70_gL")
+        "risk_factor.hemoglobin.pregnant_proportion_below_70_gL"
+    )
 
     @property
     def name(self):
-        return 'hemoglobin'
+        return "hemoglobin"
 
     @property
     def log_name(self):
-        return 'hemoglobin'
+        return "hemoglobin"
 
 
 HEMOGLOBIN = _Hemoglobin()
@@ -110,48 +113,48 @@ HEMOGLOBIN = _Hemoglobin()
 
 class __LowBirthWeightShortGestation(NamedTuple):
     # Keys that will be loaded into the artifact. must have a colon type declaration
-    EXPOSURE: str = 'risk_factor.low_birth_weight_and_short_gestation.exposure'
-    DISTRIBUTION: str = 'risk_factor.low_birth_weight_and_short_gestation.distribution'
-    CATEGORIES: str = 'risk_factor.low_birth_weight_and_short_gestation.categories'
+    EXPOSURE: str = "risk_factor.low_birth_weight_and_short_gestation.exposure"
+    DISTRIBUTION: str = "risk_factor.low_birth_weight_and_short_gestation.distribution"
+    CATEGORIES: str = "risk_factor.low_birth_weight_and_short_gestation.categories"
 
     @property
     def name(self):
-        return 'low_birth_weight_and_short_gestation'
+        return "low_birth_weight_and_short_gestation"
 
     @property
     def log_name(self):
-        return 'low birth weight and short gestation'
+        return "low birth weight and short gestation"
 
 
 LBWSG = __LowBirthWeightShortGestation()
 
 
 class __MaternalBMI(NamedTuple):
-    PREVALENCE_LOW_BMI_ANEMIC: str = 'maternal_bmi.prevalance_low_bmi_anemic'
-    PREVALENCE_LOW_BMI_NON_ANEMIC: str = 'maternal_bmi.prevalence_low_bmi_non_anemic'
+    PREVALENCE_LOW_BMI_ANEMIC: str = "maternal_bmi.prevalance_low_bmi_anemic"
+    PREVALENCE_LOW_BMI_NON_ANEMIC: str = "maternal_bmi.prevalence_low_bmi_non_anemic"
 
     @property
     def name(self):
-        return 'maternal_bmi'
+        return "maternal_bmi"
 
     @property
     def log_name(self):
-        return 'maternal BMI'
+        return "maternal BMI"
 
 
 MATERNAL_BMI = __MaternalBMI()
 
 
 class __MaternalInterventions(NamedTuple):
-    COVERAGE: str = 'maternal_interventions.coverage'
+    COVERAGE: str = "maternal_interventions.coverage"
 
     @property
     def name(self):
-        return 'maternal_interventions'
+        return "maternal_interventions"
 
     @property
     def log_name(self):
-        return 'maternal interventions'
+        return "maternal interventions"
 
 
 MATERNAL_INTERVENTIONS = __MaternalInterventions()
--- src/vivarium_gates_iv_iron/constants/data_values.py
+++ src/vivarium_gates_iv_iron/constants/data_values.py
@@ -20,11 +20,10 @@ PROBABILITY_MODERATE_MATERNAL_HEMORRHAGE = (0.85, 0.81, 0.89)
 
 # state: (mean_params, sd_params)
 HEMOGLOBIN_CORRECTION_FACTORS = {
-    models.NOT_PREGNANT_STATE: (
-        (1., 1., 1.), (1., 1., 1.)
-    ),
+    models.NOT_PREGNANT_STATE: ((1.0, 1.0, 1.0), (1.0, 1.0, 1.0)),
     models.PREGNANT_STATE: (
-        (0.919325, 0.86, 0.98), (1.032920188, 1.032920188, 1.032920188)
+        (0.919325, 0.86, 0.98),
+        (1.032920188, 1.032920188, 1.032920188),
     ),
 }
 
@@ -38,15 +37,22 @@ class _HemoglobinDistributionParameters(NamedTuple):
 
 HEMOGLOBIN_DISTRIBUTION_PARAMETERS = _HemoglobinDistributionParameters()
 
-ANEMIA_DISABILITY_WEIGHTS = {"none": 0., "mild": 0.004, "moderate": 0.052, "severe": 0.149}
+ANEMIA_DISABILITY_WEIGHTS = {
+    "none": 0.0,
+    "mild": 0.004,
+    "moderate": 0.052,
+    "severe": 0.149,
+}
 
 # tuples are: (age_start, age_end, severe_upper, moderate_upper, mild_upper)
-_hemoglobin_threshold_data = {"pregnant": [(5, 15, 80, 110, 115),
-                                           (15, 57, 70, 100, 110)],
-                              "not_pregnant": [(5, 15, 80, 110, 115),
-                                               (15, 57, 80, 110, 120)]}
-_hemoglobin_state_map = {"pregnant": models.PREGNANCY_MODEL_STATES[1:],
-                         "not_pregnant": [models.NOT_PREGNANT_STATE]}
+_hemoglobin_threshold_data = {
+    "pregnant": [(5, 15, 80, 110, 115), (15, 57, 70, 100, 110)],
+    "not_pregnant": [(5, 15, 80, 110, 115), (15, 57, 80, 110, 120)],
+}
+_hemoglobin_state_map = {
+    "pregnant": models.PREGNANCY_MODEL_STATES[1:],
+    "not_pregnant": [models.NOT_PREGNANT_STATE],
+}
 _htd = []
 for key, states in _hemoglobin_state_map.items():
     for state in states:
@@ -55,7 +61,15 @@ for key, states in _hemoglobin_state_map.items():
 
 HEMOGLOBIN_THRESHOLD_DATA = pd.DataFrame(
     _htd,
-    columns=["pregnancy_status", "sex", "age_start", "age_end", "severe", "moderate", "mild"]
+    columns=[
+        "pregnancy_status",
+        "sex",
+        "age_start",
+        "age_end",
+        "severe",
+        "moderate",
+        "mild",
+    ],
 )
 
 MATERNAL_BMI_ANEMIA_THRESHOLD = 100.0  # g/L, units of hemoglobin exposure distribution
@@ -64,16 +78,26 @@ IV_IRON_THRESHOLD = 100.0  # g/L
 IV_IRON_EFFECT_SIZE = (23, 14)  # (mean, sd) g/L
 
 # Risk Effects
-RR_MATERNAL_HEMORRHAGE_ATTRIBUTABLE_TO_HEMOGLOBIN = (3.54, 1.2, 10.4)  # (median, lower, upper) 95% CI
+RR_MATERNAL_HEMORRHAGE_ATTRIBUTABLE_TO_HEMOGLOBIN = (
+    3.54,
+    1.2,
+    10.4,
+)  # (median, lower, upper) 95% CI
 HEMOGLOBIN_SCALE_FACTOR_MODERATE_HEMORRHAGE = 0.9
 HEMOGLOBIN_SCALE_FACTOR_SEVERE_HEMORRHAGE = 0.833
 
 TMREL_HEMOGLOBIN_ON_MATERNAL_DISORDERS = 120.0
 RR_SCALAR = 10.0  # Conversion factor between hemoglobin units (g/L) and relative risk units (g/dL)
 
-RR_STILLBIRTH_PROBABILITY_ATTRIBUTABLE_TO_HEMOGLOBIN = (3.87, 1.88, 8.06)  # (median, lower, upper) 95% CI
+RR_STILLBIRTH_PROBABILITY_ATTRIBUTABLE_TO_HEMOGLOBIN = (
+    3.87,
+    1.88,
+    8.06,
+)  # (median, lower, upper) 95% CI
 
 _IFA_EFFECT_SIZE_LOWER = 4.08
 _IFA_EFFECT_SIZE_UPPER = 11.52
-_IFA_EFFECT_SIZE_SD = (_IFA_EFFECT_SIZE_UPPER - _IFA_EFFECT_SIZE_LOWER) / (2 * 1.96)  # 95% CI
+_IFA_EFFECT_SIZE_SD = (_IFA_EFFECT_SIZE_UPPER - _IFA_EFFECT_SIZE_LOWER) / (
+    2 * 1.96
+)  # 95% CI
 IFA_EFFECT_SIZE = (7.8, _IFA_EFFECT_SIZE_SD)  # (mean, sd) g/L
