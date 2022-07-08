7bba65f55f260257831627bf18ef225188e0669b
albrja <37345113+albrja@users.noreply.github.com>
2022-07-08 12:33:37 -0700

    Feature/seeds flag results (#76)
    Feature/add disaggregate_seeds flag to make_results
    
    Description
    Added a disaggregate_seeds flag (-d, --disaggregate-seeds) to make_results tool.
    - *Category*: Feature
    - *JIRA issue*: [MIC-3235](https://jira.ihme.washington.edu/browse/MIC-3235)
    - *Research reference*: Request from research team.
    
    -added flag in cli.py with default as False to preserve traditional make_results behavior.
    -Updated build_results function call with logic for new flag and passed along bool argument to make_measure data.
    -Updated make_measure_data and subsequent calls with disaggregate_seeds bool argument to handle new flag and include random seed column in results if flag is thrown.
    
    Verification and Testing
    Successfully ran make_results both with and without the new disaggregate_seeds flag with the random seed column only appearing when expected.

--- src/vivarium_gates_iv_iron/tools/make_results.py
+++ src/vivarium_gates_iv_iron/tools/make_results.py
@@ -6,7 +6,7 @@ from loguru import logger
 from vivarium_gates_iv_iron.results_processing import process_results
 
 
-def build_results(output_file: str, single_run: bool):
+def build_results(output_file: str, single_run: bool, disaggregate_seeds: bool):
     output_file = Path(output_file)
     measure_dir = output_file.parent / "count_data"
     if measure_dir.exists():
@@ -22,9 +22,10 @@ def build_results(output_file: str, single_run: bool):
     logger.info(
         f"Filtered {rows - new_rows} from data due to incomplete information.  {new_rows} remaining."
     )
-    data = process_results.aggregate_over_seed(data)
+    if not disaggregate_seeds:
+        data = process_results.aggregate_over_seed(data)
     logger.info(f"Computing raw count and proportion data.")
-    measure_data = process_results.make_measure_data(data)
+    measure_data = process_results.make_measure_data(data, disaggregate_seeds)
     logger.info(f"Writing raw count and proportion data to {str(measure_dir)}")
     measure_data.dump(measure_dir)
     logger.info("**DONE**")
